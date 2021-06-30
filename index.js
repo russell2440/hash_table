b42af0e01a4c2045a7f734a90d6137372bbc21a9
russell2440 <85845760+russell2440@users.noreply.github.com>
2021-06-30 01:23:15 +0000

    Fillout 1

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 class hash_table{
   constructor(size){
-    
+    this.data = new Array(size);
   }
   hash_fn(key){
 
+    let hash = 0;
+
+    console.log('hash_fn at',hash);
+    return hash;
   }
+
   set(key,value){
-
+    let index = hash_fn(key);
+    console.log('set at',index);
+    this.data[index] = value;
   }
+  
   get(key){
-
+    let index = hash_fn(key);
+    console.log('get at',index);
+    let value = this.data[index];
+    console.log('get value',value);
+    return(value);
   }
 }
 
