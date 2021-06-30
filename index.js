19ccf76b2954f8ea367534b4c84433e9200051d5
russell2440 <85845760+russell2440@users.noreply.github.com>
2021-06-30 13:24:51 +0000

    added 'this' to solve issues

--- index.js
+++ index.js
@@ -14,17 +14,17 @@ class hash_table{
 
   set(key,value){
     let index = 0;
-    index = hash_fn(key);
+    index = this.hash_fn(key);
     console.log('set at',index);
     this.data[index] = value;
   }
 
   get(key){
     let index = 0;
-    index = hash_fn(key);
+    index = this.hash_fn(key);
     console.log('get at',index);
     let value = this.data[index];
-    console.log('get value',value);
+    console.log('get value',value,'\n');
     return(value);
   }
 }
@@ -34,3 +34,5 @@ ht.set('grapes',1000);
 ht.get('grapes');
 ht.set('apples',20000);
 ht.get('apples');
+ht.set('peach',40000);
+ht.get('peach');
