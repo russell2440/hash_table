a5099d157cbab43a0b7b3943300bbae6080ee2a1
russell2440 <85845760+russell2440@users.noreply.github.com>
2021-06-30 01:11:58 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+class hash_table{
+  constructor(size){
+    
+  }
+  hash_fn(key){
+
+  }
+  set(key,value){
+
+  }
+  get(key){
+
+  }
+}
+
+const ht = new hash_table(20);
+ht.set('grapes',1000);
+ht.get('grapes');
+ht.set('apples',20000);
+ht.get('apples');
